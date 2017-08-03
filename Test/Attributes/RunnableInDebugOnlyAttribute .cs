c0b2db4bf6337c2edc6861ba6d02ddf3ffaa896d
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-03 09:07:15 +0100

    Added fluent asserts

---
+++ Test/Attributes/RunnableInDebugOnlyAttribute .cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace test.Attributes
+{
+    //see https://lostechies.com/jimmybogard/2013/06/20/run-tests-explicitly-in-xunit-net/ 
+    public class RunnableInDebugOnlyAttribute : FactAttribute
+    {
+        public RunnableInDebugOnlyAttribute()
+        {
+            if (!Debugger.IsAttached)
+            {
+                Skip = "Only running in interactive mode.";
+            }
+        }
+    }
+}
