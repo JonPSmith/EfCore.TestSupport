136002542b18b5159ad9c52154f2420b207f23f0
Jon Smith <jon.smith@selectiveanalytics.com>
2017-08-16 19:51:37 +0100

    Added StartWith and EndsWith

---
+++ TestSupport/Assert.Extensions/ExtraStringAssertionExtensions.cs
@@ -0,0 +1,20 @@
+﻿// // Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// // Licensed under MIT licence. See License.txt in the project root for license information.
+
+namespace Xunit.Extensions.AssertExtensions
+{
+    public static class ExtraStringAssertionExtensions
+    {
+        public static void ShouldStartWith(this string actualString,
+            string expectedStartString)
+        {
+            Assert.StartsWith(expectedStartString, actualString);
+        }
+
+        public static void ShouldEndWith(this string actualString,
+            string expectedEndString)
+        {
+            Assert.EndsWith(expectedEndString, actualString);
+        }
+    }
+}
