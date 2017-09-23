6b15e0addb55eeced8fdcd95103ed613e21894d3
Jon Smith <jon.smith@selectiveanalytics.com>
2017-09-23 14:33:10 +0100

    Fixed bug when running all

--- Test/UnitTests/DataLayer/TestEfLogging.cs
+++ Test/UnitTests/DataLayer/TestEfLogging.cs
@@ -1,16 +1,23 @@
-﻿// // Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
-// // Licensed under MIT licence. See License.txt in the project root for license information.
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
 using DataLayer.EfCode;
 using TestSupport.EfHelpers;
 using Xunit;
+using Xunit.Abstractions;
 using Xunit.Extensions.AssertExtensions;
 
 namespace Test.UnitTests.DataLayer
 {
     public class TestEfLogging
     {
+        private readonly ITestOutputHelper _output;
+
+        public TestEfLogging(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         [Fact]
         public void TestEfCoreLogging1()
@@ -24,7 +31,11 @@ namespace Test.UnitTests.DataLayer
                 context.Database.EnsureCreated();
 
                 //VERIFY
-                logs.Count.ShouldEqual(11);
+                foreach (var log in logs)
+                {
+                    _output.WriteLine(log.ToString());
+                }
+                logs.Count.ShouldBeInRange(11, 20);
             }
         }
 
