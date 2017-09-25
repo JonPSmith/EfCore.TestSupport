869e74061455843147613c7be6543503a9ac0818
Jon Smith <jon.smith@selectiveanalytics.com>
2017-09-25 15:36:44 +0100

    added comments

--- TestSupport/Attributes/RunnableInDebugOnlyAttribute .cs
+++ TestSupport/Attributes/RunnableInDebugOnlyAttribute .cs
@@ -3,9 +3,17 @@ using Xunit;
 
 namespace TestSupport.Attributes
 {
-    //see https://lostechies.com/jimmybogard/2013/06/20/run-tests-explicitly-in-xunit-net/ 
+    /// <summary>
+    /// Useful attribute for stopping a test from being run
+    /// see https://lostechies.com/jimmybogard/2013/06/20/run-tests-explicitly-in-xunit-net/ 
+    /// </summary>
     public class RunnableInDebugOnlyAttribute : FactAttribute
     {
+        /// <summary>
+        /// By putting this attribute on a test instead of the normal [Fact] attribute will mean the 
+        /// test will only run if in debug mode.
+        /// This is useful for stopping unit tests that should not be run in the normal run of unit tests
+        /// </summary>
         public RunnableInDebugOnlyAttribute()
         {
             if (!Debugger.IsAttached)
