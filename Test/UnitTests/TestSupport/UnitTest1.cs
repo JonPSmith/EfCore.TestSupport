b0b09eaf5250871522f9da48506740361608bc47
Jon Smith <jon.smith@selectiveanalytics.com>
2017-09-26 14:07:32 +0100

    Added annotations

--- Test/UnitTests/TestSupport/UnitTest1.cs
+++ Test/UnitTests/TestSupport/UnitTest1.cs
@@ -1,17 +1,28 @@
 using Xunit;
+using Xunit.Extensions.AssertExtensions;
 
 namespace Test.UnitTests.TestSupport
 {
     public class UnitTest1
     {
-        [Fact]
-        public void DemoTest()
+        [Fact]                  //#A
+        public void DemoTest() //#B
         {
-            //SETUP
+            //SETUP                  
+            const int someValue = 1; //#C
 
             //ATTEMPT
+            var result = someValue * 2; //#D
 
             //VERIFY
+            result.ShouldEqual(2); //#E
         }
+        /*****************************************************
+        #A The [Fact] attribute tells the unit test runner that this method is an xUnit unit test that should be run
+        #B The method must be public. It should return void, or if you are running async methods, then it should return "async Task" 
+        #C Typically you put code here that sets up the data and/or environment for the unit test
+        #D This is where you run the code you want to test
+        #E And here is where you put the test(s) to check that the result of your test is correct
+         * ***************************************************/
     }
 }
--- TestSupport/Helpers/AppSettings.cs
+++ TestSupport/Helpers/AppSettings.cs
@@ -20,14 +20,22 @@ namespace TestSupport.Helpers
         /// This will look for a appsettings.json file in the top level of the calling assembly and read content
         /// </summary>
         /// <returns></returns>
-        public static IConfigurationRoot GetConfiguration()
+        public static IConfigurationRoot GetConfiguration() //#A
         {
-            var callingProjectPath = TestFileHelpers.GetCallingAssemblyTopLevelDirectory();
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(callingProjectPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
-            return builder.Build();
+            var callingProjectPath =                      //#B
+                TestFileHelpers                           //#B
+                   .GetCallingAssemblyTopLevelDirectory();//#B
+            var builder = new ConfigurationBuilder()               //#C
+                .SetBasePath(callingProjectPath)                   //#C
+                .AddJsonFile("appsettings.json", optional: false); //#C
+            return builder.Build(); //#D
         }
+        /******************************************************************
+        #A This method returns an IConfigurationRoot, form which I can use methods, such as GetConnectionString("ConnectionName"), to access the configation information
+        #B In my TestSupport library I have a method that returns the absolute path of the calling assembly's top level directory. That will be the assembly that you ar running your tests in
+        #C I then use ASP.NET Core's ConfigurationBuilder to read that appsettings.json file
+        #D Finally I call the Build() method, which returns the IConfigurationRoot type
+         * ***************************************************************/
 
         /// <summary>
         /// This creates a unique database name based on the test class name, and an optional extra name
