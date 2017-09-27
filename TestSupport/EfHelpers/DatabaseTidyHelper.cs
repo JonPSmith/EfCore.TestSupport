0bf2d0faefe0c48cf1a4f85bea04db9bf8c741cf
Jon Smith <jon.smith@selectiveanalytics.com>
2017-09-27 16:49:10 +0100

    Reorganised methods

--- TestSupport/EfHelpers/DatabaseTidyHelper.cs
+++ TestSupport/EfHelpers/DatabaseTidyHelper.cs
@@ -12,17 +12,13 @@ namespace TestSupport.EfHelpers
     public static class DatabaseTidyHelper
     {
         //Note that there is no initial catalog
-        internal const string LocalHostConnectionString = "Server=(localdb)\\mssqllocaldb;Initial Catalog=;Trusted_Connection=True;";
+        private const string LocalHostConnectionString = "Server=(localdb)\\mssqllocaldb;Initial Catalog=;Trusted_Connection=True;";
 
-        internal static void DeleteDatabase(this string databaseName)
-        {
-            if (LocalHostConnectionString.ExecuteRowCount("sys.databases", $"WHERE [Name] = '{databaseName}'") == 1)
-                LocalHostConnectionString.ExecuteNonQuery("DROP DATABASE [" + databaseName + "]");
-            if (LocalHostConnectionString.ExecuteRowCount("sys.databases", $"WHERE [Name] = '{databaseName}'") == 1)
-                //it failed
-                throw new InvalidOperationException($"Failed to deleted {databaseName}. Did you have SSMS open or something?");
-        }
-
+        /// <summary>
+        /// This will delete all the databases that start with the database name in the default connection string
+        /// WARNING: This will delete multiple databases - make sure your DefaultConnection database name is unique!!!
+        /// </summary>
+        /// <returns>Number of databases deleted</returns>
         public static int DeleteAllUnitTestDatabases()
         {
             var config = AppSettings.GetConfiguration();
@@ -37,6 +33,12 @@ namespace TestSupport.EfHelpers
             return databaseNamesToDelete.Count;
         }
 
+        /// <summary>
+        /// This returns all the matching databases that start with the startsWith parameter 
+        /// </summary>
+        /// <param name="startsWith"></param>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
         public static List<string> GetAllMatchingDatabases(this string startsWith,
             string connectionString = LocalHostConnectionString)
         {
@@ -56,6 +58,15 @@ namespace TestSupport.EfHelpers
                 }
             }
             return result;
+        }
+
+        private static void DeleteDatabase(this string databaseName)
+        {
+            if (LocalHostConnectionString.ExecuteRowCount("sys.databases", $"WHERE [Name] = '{databaseName}'") == 1)
+                LocalHostConnectionString.ExecuteNonQuery("DROP DATABASE [" + databaseName + "]");
+            if (LocalHostConnectionString.ExecuteRowCount("sys.databases", $"WHERE [Name] = '{databaseName}'") == 1)
+                //it failed
+                throw new InvalidOperationException($"Failed to deleted {databaseName}. Did you have SSMS open or something?");
         }
     }
 }
