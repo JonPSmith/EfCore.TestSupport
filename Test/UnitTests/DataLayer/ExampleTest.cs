e9fd439cb4d5f970209dfaa2d521180d89215064
Jon Smith <jon.smith@selectiveanalytics.com>
2017-10-08 15:46:13 +0100

    Added example test code/image

---
+++ Test/UnitTests/DataLayer/ExampleTest.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2017 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT licence. See License.txt in the project root for license information.
+
+using System.Data.SqlClient;
+using System.Linq;
+using DataLayer.EfClasses;
+using DataLayer.EfCode;
+using Microsoft.EntityFrameworkCore;
+using Test.Helpers;
+using TestSupport.EfHelpers;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Test.UnitTests.DataLayer
+{
+    public class ExampleTest 
+    {
+        private readonly ITestOutputHelper _output;
+
+        public ExampleTest(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void TestExample()
+        {
+            //SETUP
+            var options = this
+                .CreateUniqueClassOptions<EfCoreContext>();
+            using (var context = new EfCoreContext(options))
+            {
+                context.CreateEmptyViaWipe();
+                var logs = context.SetupLogging();
+
+                //ATTEMPT
+                context.Add(new Book {Title = "New Book"});
+                context.SaveChanges();
+
+                //VERIFY
+                context.Books.Count().ShouldEqual(1);
+                foreach (var log in logs.ToList())
+                {                                    
+                    _output.WriteLine(log.ToString());
+                }                                     
+            }
+        }
+
+    }
+}
