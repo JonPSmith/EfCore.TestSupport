09cf7f09bc4c32fc4d33eaf24f628de7c16c38f5
Jon Smith <jon.smith@selectiveanalytics.com>
2017-10-13 15:34:11 +0100

    Added annotations for book

--- TestSupport/EfHelpers/LogOutput.cs
+++ TestSupport/EfHelpers/LogOutput.cs
@@ -7,32 +7,48 @@ using Microsoft.Extensions.Logging;
 
 namespace TestSupport.EfHelpers
 {
-    public class LogOutput
+    public class LogOutput //#A
     {
-        private const string EfCoreEventIdStartWith = "Microsoft.EntityFrameworkCore";
+        private const string EfCoreEventIdStartWith //#B
+            = "Microsoft.EntityFrameworkCore";      //#B
 
-        public LogLevel LogLevel { get; private set; }
-        public EventId EventId { get; private set; }
-        public string Message { get; private set; }
+        public LogLevel LogLevel { get; } //#C
+        public EventId EventId { get; }  //#D
+        public string Message { get; }  //#E
 
         /// <summary>
         /// This returns the last part of an EF Core EventId name, or null if the eventId is not an EF Core one
         /// </summary>
-        public string EfEventIdLastName => 
-            EventId.Name?.StartsWith(EfCoreEventIdStartWith) == true
-                ? EventId.Name.Split('.').Last()
-                : null;
+        public string EfEventIdLastName => //#F
+            EventId.Name?.StartsWith(                //#G
+                     EfCoreEventIdStartWith) == true //#G
+                ? EventId.Name.Split('.').Last()     //#G
+                : null;                              //#G
 
-        public LogOutput(LogLevel logLevel, EventId eventId, string message)
-        {
-            LogLevel = logLevel;
-            EventId = eventId;
-            Message = message;
-        }
+        internal LogOutput(LogLevel logLevel,//#H 
+            EventId eventId, string message) //#H
+        {                                    //#H
+            LogLevel = logLevel;             //#H
+            EventId = eventId;               //#H
+            Message = message;               //#H
+        }                                    //#H
 
-        public override string ToString()
+        public override string ToString() //#G
         {
-            return $"{LogLevel},{EfEventIdLastName}: {Message}";
+            return 
+                $"{LogLevel},{EfEventIdLastName}: " +
+                Message;
         }
     }
+    /***************************************************************
+    #A This class holds each log captured from EF Core
+    #B I use this string to identify logs that were produced by EF Core
+    #C This holds what LogLevel the log was reported at, for istance, Information, Warning, Error
+    #D This holds the EventId - useful because EF Core 2.0.0 has named events
+    #E This is the logged message
+    #F This property returns the last part of the name, but only if its an EF Core log. Useful, as it is a quick way to identify specific events
+    #G This gets either the last part of the EF Core eventid name, or null if not EF Core
+    #H The constructor for the class
+    #I Typically you will show the logs as text, so the ToString method returns a useful string
+     * **************************************************************/
 }
--- TestSupport/EfHelpers/LogSetupHelper.cs
+++ TestSupport/EfHelpers/LogSetupHelper.cs
@@ -13,13 +13,31 @@ namespace TestSupport.EfHelpers
 {
     public static class LogSetupHelper
     {
-        public static List<LogOutput> SetupLogging(this DbContext context, LogLevel logLevel = LogLevel.Information)
+        /// <summary>
+        /// This extension method will start logging the output of EF Core
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="logLevel"></param>
+        /// <returns>It passes back a reference to a list of Logs that EF Core will add to as the unit test progresses</returns>
+        public static List<LogOutput> SetupLogging( //#A
+            this DbContext context, //#B
+            LogLevel logLevel = LogLevel.Information) //#C
         {
-            var logs = new List<LogOutput>();
-            var loggerFactory = context.GetService<ILoggerFactory>();
-            loggerFactory.AddProvider(new MyLoggerProvider(logs, logLevel));
-            return logs;
+            var logs = new List<LogOutput>(); //#D
+            var loggerFactory = context        //#E
+                .GetService<ILoggerFactory>(); //#E
+            loggerFactory.AddProvider(                 //#F
+                new MyLoggerProvider(logs, logLevel)); //#F
+            return logs; //#G
         }
+        /********************************************************************
+        #A This is an extension method that will start capturing logging produced by EF Core
+        #B I need the current application's DbContext to work with
+        #C It default to capturing logs at Information level or above, but you can change that
+        #D I create a list of LogOutput, which is class I created to hold the 
+        #E I get the ILoggerFactory service from EF Core
+        #D
+         * *******************************************************************/
 
         public static IEnumerable<string> DecodedLogs(List<LogOutput> logs)
         {
