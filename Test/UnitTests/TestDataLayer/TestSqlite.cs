03e62513a09c9fb651817dae2cedc47cf349f0cb
Jon P Smith <jon.smith@selectiveanalytics.com>
2020-12-05 10:00:43 +0000

    Fixed unit test to EF Core 5 level

--- Test/UnitTests/TestDataLayer/TestSqlite.cs
+++ Test/UnitTests/TestDataLayer/TestSqlite.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using DataLayer.BookApp.EfCode;
 using DataLayer.MyEntityDb;
 using DataLayer.MyEntityDb.EfCompareDbs;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Test.Helpers;
 using TestSupport.EfHelpers;
@@ -156,17 +157,17 @@ namespace Test.UnitTests.TestDataLayer
         }
 
         [Fact]
-        public void TestSqlLiteDoesNotSupportComputedCol()
+        public void TestSqlLiteComputedColDifferent()
         {
             //SETUP
             var options = SqliteInMemory.CreateOptions<MyEntityComputedColDbContext>();
             using (var context = new MyEntityComputedColDbContext(options))
             {
                 //ATTEMPT
-                var ex = Assert.Throws<NotSupportedException>(() => context.Database.EnsureCreated());
+                var ex = Assert.Throws<SqliteException>(() => context.Database.EnsureCreated());
 
                 //VERIFY
-                ex.Message.ShouldStartWith("SQLite doesn't support computed columns.");
+                ex.Message.ShouldStartWith("SQLite Error 1: 'no such function: getutcdate'.");
             }
         }
 
