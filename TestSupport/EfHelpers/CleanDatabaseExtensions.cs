b666fc8cf7d7b710d99544aec1a752bfe5255e72
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-11-18 14:48:54 +0000

    Moved PostgreSqlEnsureClean code to internal class

--- TestSupport/EfHelpers/CleanDatabaseExtensions.cs
+++ TestSupport/EfHelpers/CleanDatabaseExtensions.cs
@@ -5,7 +5,6 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Npgsql;
 using TestSupport.EfHelpers.Internal;
 
 namespace TestSupport.EfHelpers
@@ -33,49 +32,6 @@ namespace TestSupport.EfHelpers
                 databaseFacade.FasterPostgreSqlEnsureClean(setUpSchema);
             else
                 throw new InvalidOperationException("The EnsureClean method only works with SQL Server or PostgreSQL databases.");
-        }
-
-        private static void FasterPostgreSqlEnsureClean(this DatabaseFacade databaseFacade, bool setUpSchema = true)
-        {
-            var connectionString = databaseFacade.GetDbConnection().ConnectionString;
-            if (connectionString.DatabaseExists())
-            {
-                var conn = new NpgsqlConnection(connectionString);
-                conn.Open();
-
-                var dropPublicSchemaCommand = new NpgsqlCommand
-                {
-                    Connection = conn,
-                    CommandText = @"
-DO $$
-DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT nspname FROM pg_namespace WHERE nspname NOT IN ('pg_toast', 'pg_catalog', 'information_schema'))
-        LOOP
-            EXECUTE 'DROP SCHEMA ' || quote_ident(r.nspname) || ' CASCADE';
-        END LOOP;
-    EXECUTE 'CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public';
-END $$"
-                };
-                dropPublicSchemaCommand.ExecuteNonQuery();
-            }
-
-            if (setUpSchema)
-                databaseFacade.EnsureCreated();
-        }
-
-        private static bool DatabaseExists(this string connectionString)
-        {
-            var builder = new NpgsqlConnectionStringBuilder(connectionString);
-            var orgDbStartsWith = builder.Database;
-            builder.Database = "postgres";
-            var newConnectionString = builder.ToString();
-            using var conn = new NpgsqlConnection(newConnectionString);
-            conn.Open();
-
-            using var cmd = new NpgsqlCommand($"SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname='{orgDbStartsWith}'", conn);
-            return (long)cmd.ExecuteScalar() == 1;
         }
     }
 }
---
+++ TestSupport/EfHelpers/Internal/PostgreSqlDropSchemaEnsureClean.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Npgsql;
+
+namespace TestSupport.EfHelpers.Internal
+{
+    internal static class PostgreSqlDropSchemaEnsureClean
+    {
+        /// <summary>
+        /// This uses the "DROP SCHEMA" approach (see https://stackoverflow.com/a/13823560/1434764) 
+        /// to remove all the tables, extensions, functions, collations.
+        /// The SQL in this method was provided by Shay Rojansky, github @roji
+        /// </summary>
+        /// <param name="databaseFacade"></param>
+        /// <param name="setUpSchema"></param>
+        public static void FasterPostgreSqlEnsureClean(this DatabaseFacade databaseFacade, bool setUpSchema = true)
+        {
+            var connectionString = databaseFacade.GetDbConnection().ConnectionString;
+            if (connectionString.DatabaseExists())
+            {
+                var conn = new NpgsqlConnection(connectionString);
+                conn.Open();
+
+                var dropPublicSchemaCommand = new NpgsqlCommand
+                {
+                    Connection = conn,
+                    CommandText = @"
+DO $$
+DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT nspname FROM pg_namespace WHERE nspname NOT IN ('pg_toast', 'pg_catalog', 'information_schema'))
+        LOOP
+            EXECUTE 'DROP SCHEMA ' || quote_ident(r.nspname) || ' CASCADE';
+        END LOOP;
+    EXECUTE 'CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO postgres; GRANT ALL ON SCHEMA public TO public';
+END $$"
+                };
+                dropPublicSchemaCommand.ExecuteNonQuery();
+            }
+
+            if (setUpSchema)
+                databaseFacade.EnsureCreated();
+        }
+
+        private static bool DatabaseExists(this string connectionString)
+        {
+            var builder = new NpgsqlConnectionStringBuilder(connectionString);
+            var orgDbStartsWith = builder.Database;
+            builder.Database = "postgres";
+            var newConnectionString = builder.ToString();
+            using var conn = new NpgsqlConnection(newConnectionString);
+            conn.Open();
+
+            using var cmd = new NpgsqlCommand($"SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname='{orgDbStartsWith}'", conn);
+            return (long)cmd.ExecuteScalar() == 1;
+        }
+    }
+}
